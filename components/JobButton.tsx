e1d6e14d34f0e36f8d5a863e9388690c504e199f
tranhuyviet <vietth@msn.com>
2022-03-05 09:41:11 +0200

    Change UI of Jobbutton

--- components/JobButton.tsx
+++ components/JobButton.tsx
@@ -3,13 +3,22 @@ import Image from 'next/image'
 interface IJobButton {
     name: string
     urlIcon: string
+    point: number
 }
 
-const JobButton = ({ name, urlIcon }: IJobButton) => {
+const JobButton = ({ name, urlIcon, point }: IJobButton) => {
     return (
-        <div className="flex flex-col items-center">
-            <Image src={urlIcon} className="job-icons" width={60} height={60} alt="mop" />
-            <p className="mt-2 text-lg">{name}</p>
+        <div className="shadow-lg w-full h-full pb-full relative rounded-2xl bg-green-100 hover:cursor-pointer">
+            <div className="flex flex-col items-center justify-center p-4">
+                <Image src={urlIcon} className="job-icons" width={60} height={60} alt="mop" />
+                <p className="mt-2 text-lg">{name}</p>
+            </div>
+            <div className="absolute right-2 top-2">
+                <div className="relative animate-pulse">
+                    <Image src="/images/star.png" className="rotate-12" width={36} height={36} alt="mop" />
+                    <p className="absolute right-[14px] top-2">{point}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello App</h1>
-      <div>
-        <div className="flex justify-center space-x-4">
-
-          <JobButton name="House Cleaning" urlIcon="/images/mop.png" />
-          <JobButton name="Dishwasher" urlIcon="/images/dishwasher.png" />
-          <JobButton name="Cleanup Toys" urlIcon="/images/toys.png" />
+      <div className="p-4">
+        <div className="grid grid-cols-2 gap-4 place-items-center">
+          <JobButton name="House Cleaning" urlIcon="/images/mop.png" point={2} />
+          <JobButton name="Dishwasher" urlIcon="/images/dishwasher.png" point={1} />
+          <JobButton name="Cleanup Toys" urlIcon="/images/toys.png" point={1} />
+          <JobButton name="Take Care" urlIcon="/images/baby-boy.png" point={1} />
+          <JobButton name="Do Homework" urlIcon="/images/homework.png" point={1} />
         </div>
 
       </div>
