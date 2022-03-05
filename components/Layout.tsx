cdd8e9ac6632f59e52f2953ddaabc1002ffa9f06
tranhuyviet <vietth@msn.com>
2022-03-05 10:01:55 +0200

    Add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactChild } from 'react'
+import Navbar from './Navbar'
+
+interface IProps {
+    children: ReactChild
+}
+
+const Layout = ({ children }: IProps) => {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout
