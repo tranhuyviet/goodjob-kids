9671ea699f327419e2676aef4c4ab72492a16006
tranhuyviet <vietth@msn.com>
2022-04-02 12:42:57 +0300

    Fix deploy 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ import '../styles/globals.css'
 import axios from 'axios'
 
 // axios seting
-const url = process.env.NODE_ENV === 'production' ? 'https://goodjob-kids.vercel.app/api' : 'http://localhost:3000/api'
+// const url = process.env.NODE_ENV === 'production' ? 'https://goodjob-kids.vercel.app/api' : 'http://localhost:3000/api'
+const url = process.env.NODE_ENV === 'production' ? 'https://goodjob-kids-git-dev-tranhuyviet.vercel.app/api' : 'http://localhost:3000/api'
 axios.defaults.baseURL = url
 axios.defaults.withCredentials = true
 axios.defaults.headers.post['Content-Type'] = 'application/json'
